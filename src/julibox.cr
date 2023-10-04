ac9cd11d4415376d8996690b77b39afd4b87cc98
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-10-04 22:55:21 +0300

    wip proxy server

--- src/julibox.cr
+++ src/julibox.cr
@@ -1,18 +1,105 @@
+require "uri"
+require "http"
+require "http/server/handler"
 require "http/web_socket"
 
-ws_handler = HTTP::WebSocketHandler.new do |ws, ctx|
-  # wss://i-08ed2a462549e47c0.play.jackboxgames.com/api/v2/rooms/WYJX/play?role=player&name=GIRLTHING&format=json&user-id=aaac9766-7291-4d0e-bca5-f9e253bf7489
-  # TODO
-  proxy = HTTP::WebSocket.new("websocket.example.com", "/chat", tls: true)
+log_handler = HTTP::LogHandler.new
 
-  ws.on_ping { proxy.ping }
-  ws.on_message { |msg| proxy.send(msg) }
-  ws.on_close { |close_code, msg| proxy.close(close_code, msg) }
-  
-  proxy.on_pong { ws.pong }
-  proxy.on_message { |msg| ws.send(msg) }
-  proxy.on_close { |close_code, msg| ws.close(close_code, msg) }
+ws_handler = HTTP::WebSocketHandler.new do |ws, ctx|
+  ws.on_ping { ws.pong }
+  ws.on_message do |msg|
+    puts msg
+  end
 end
 
-server = HTTP::Server.new [ws_handler]
+class IndexHandler
+  include HTTP::Handler
 
+  def call(context)
+    if context.request.path == "/"
+      context.request.path = "/index.html"
+    end
+    call_next(context)
+  end
+end
+
+index_handler = IndexHandler.new
+
+file_handler = HTTP::StaticFileHandler.new("src/assets/", directory_listing = false)
+
+def source_url
+  "http://127.0.0.1:8080"
+end
+
+class ProxyHandler
+  include HTTP::Handler
+
+  def initialize(@target : String)
+  end
+
+  def should_intercept_body?(request : HTTP::Request)
+    if request.path.ends_with?(".js")
+      true
+    end
+  end
+
+  def rewrite_body(request : HTTP::Request, body : String) : String
+    if request.path.ends_with?(".js")
+      body = body.sub("https://bundles.jackbox.tv/", source_url + "/bundles/")
+      body = body.sub("ecast.jackboxgames.com", source_url.sub("http://", "").sub("https://", ""))
+    end
+    body
+  end
+
+  def transform_req_path(path : String)
+    if path.starts_with?("/bundles/")
+      return "https://bundles.jackbox.tv/" + path.lchop("/bundles/")
+    end
+    "https://#{@target}#{path}"
+  end
+
+  def call(context)
+    req = context.request.dup
+
+    # redirect
+    orig_url = transform_req_path(req.path)
+    # we don't want to look stupid walking up to jackbox and going "hey is this 127.0.0.1:8080"
+    uri = URI.parse(orig_url)
+    req.headers["Host"] = uri.host.not_nil!
+    req.headers["Origin"] = "https://#{@target}"
+    req.headers["Accept-Encoding"] = "" # too lazy to ungzip
+
+    # should we intercept the body?
+    if should_intercept_body?(req)
+      response = HTTP::Client.exec(req.method, orig_url, req.headers, req.body)
+      
+      # proxy everything but the body
+      context.response.status = response.status
+      context.response.headers.merge!(response.headers)
+
+      # intercept the body
+      new_body = rewrite_body(req, response.body)
+      context.response << new_body
+
+      context.response.close
+    else
+       HTTP::Client.exec(req.method, orig_url, req.headers, req.body) do |response|
+        # proxy everything
+        context.response.status = response.status
+        context.response.headers.merge!(response.headers)
+
+        IO.copy(response.body_io, context.response)
+
+        context.response.close
+      end
+    end
+  end
+end
+
+asset_proxy_handler = ProxyHandler.new "jackbox.tv"
+
+server = HTTP::Server.new [log_handler, ws_handler, index_handler, file_handler, asset_proxy_handler]
+
+address = server.bind_tcp 8080
+puts "Listening on http://#{address}"
+server.listen
