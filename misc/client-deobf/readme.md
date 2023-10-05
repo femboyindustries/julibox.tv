f2f6a2ecdd93069b2ae8ecb4e1af47b1965e46c6
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-10-05 23:14:20 +0300

    dumbass stupid fucking mocking fuck fuck fuck fuck

--- misc/client-deobf/readme.md
+++ misc/client-deobf/readme.md
@@ -1,1 +1,4 @@
 Initial pass done with [synchrony](https://github.com/relative/synchrony)
+
+`5081ecbd.js`: Initial client code, loaded in index.html. Sets up the module system
+`b14311ea.js`: Full version of the client code, loaded by `5081ecd.js`. Sets up all connection logic, loads other JS files based on game
--- src/julibox.cr
+++ src/julibox.cr
@@ -1,14 +1,30 @@
 require "uri"
+require "json"
 require "http"
 require "http/server/handler"
 require "http/web_socket"
 
-log_handler = HTTP::LogHandler.new
+class WSSender
+  @pc = 230
+  
+  def initialize(@ws : HTTP::WebSocket)
+  end
+
+  def send(opcode : String, result)
+    @ws.send({
+      "pc" => @pc,
+      "opcode" => opcode,
+      "result" => result
+    }.to_json)
+    @pc += 1
+  end
+end
 
 ws_handler = HTTP::WebSocketHandler.new do |ws, ctx|
-  ws.on_ping { ws.pong }
-  ws.on_message do |msg|
-    puts msg
+  d = WSSender.new ws
+
+  JSON.parse(File.read("src/nopus-opus_packets.json")).as_a.each do |obj|
+    d.send(obj["opcode"].as_s, obj["result"])
   end
 end
 
@@ -23,10 +39,11 @@ class IndexHandler
   end
 end
 
-index_handler = IndexHandler.new
-
 file_handler = HTTP::StaticFileHandler.new("src/assets/", directory_listing = false)
 
+def source_origin
+  "127.0.0.1:8080"
+end
 def source_url
   "http://127.0.0.1:8080"
 end
@@ -38,22 +55,46 @@ class ProxyHandler
   end
 
   def should_intercept_body?(request : HTTP::Request)
-    if request.path.ends_with?(".js")
-      true
-    end
+    request.path.ends_with?(".js") || request.path.starts_with?("/api/")
   end
 
   def rewrite_body(request : HTTP::Request, body : String) : String
     if request.path.ends_with?(".js")
       body = body.sub("https://bundles.jackbox.tv/", source_url + "/bundles/")
-      body = body.sub("ecast.jackboxgames.com", source_url.sub("http://", "").sub("https://", ""))
+      body = body.sub("ecast.jackboxgames.com", source_origin)
+      if source_url.starts_with?("http://")
+        body = body.sub("scheme:\"https\"", "scheme:\"http\"")
+        body = body.sub("e.scheme:this.scheme=\"wss\"", "e.scheme:this.scheme=\"ws\"")
+      end
     end
+
+    if request.path.starts_with?("/api/")
+      if request.path.starts_with?("/api/v2/rooms/")
+        json = JSON.parse(File.read("src/nopus-opus_game.json"))
+      else
+        json = JSON.parse(body)
+        if !json["ok"].as_bool || !(json["body"]?.try &.as_h?)
+          return body
+        end
+      end
+
+      if request.path.starts_with?("/api/v2/rooms/")
+        json["body"].as_h["host"] = JSON::Any.new "#{source_origin}/#{json["body"]["host"]}"
+        # todo: audience support (is this really necessary)
+      end
+
+      return json.to_json
+    end
+
     body
   end
 
   def transform_req_path(path : String)
     if path.starts_with?("/bundles/")
       return "https://bundles.jackbox.tv/" + path.lchop("/bundles/")
+    end
+    if path.starts_with?("/api/v2")
+      return "https://ecast.jackboxgames.com" + path
     end
     "https://#{@target}#{path}"
   end
@@ -67,26 +108,33 @@ class ProxyHandler
     uri = URI.parse(orig_url)
     req.headers["Host"] = uri.host.not_nil!
     req.headers["Origin"] = "https://#{@target}"
-    req.headers["Accept-Encoding"] = "" # too lazy to ungzip
 
     # should we intercept the body?
     if should_intercept_body?(req)
+      req.headers["Accept-Encoding"] = "" # too lazy to ungzip
+
       response = HTTP::Client.exec(req.method, orig_url, req.headers, req.body)
       
       # proxy everything but the body
       context.response.status = response.status
       context.response.headers.merge!(response.headers)
+      # fuck cors
+      context.response.headers["Access-Control-Allow-Origin"] = "*"
 
       # intercept the body
       new_body = rewrite_body(req, response.body)
+      # DON'T FORGET
+      context.response.content_length = new_body.bytesize
       context.response << new_body
 
       context.response.close
     else
-       HTTP::Client.exec(req.method, orig_url, req.headers, req.body) do |response|
+      HTTP::Client.exec(req.method, orig_url, req.headers, req.body) do |response|
         # proxy everything
         context.response.status = response.status
         context.response.headers.merge!(response.headers)
+        # fuck cors
+        context.response.headers["Access-Control-Allow-Origin"] = "*"
 
         IO.copy(response.body_io, context.response)
 
@@ -98,7 +146,7 @@ end
 
 asset_proxy_handler = ProxyHandler.new "jackbox.tv"
 
-server = HTTP::Server.new [log_handler, ws_handler, index_handler, file_handler, asset_proxy_handler]
+server = HTTP::Server.new [HTTP::ErrorHandler.new, HTTP::LogHandler.new, ws_handler, HTTP::CompressHandler.new, IndexHandler.new, file_handler, asset_proxy_handler]
 
 address = server.bind_tcp 8080
 puts "Listening on http://#{address}"
