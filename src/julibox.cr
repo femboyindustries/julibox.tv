2a7fca9446d962608b55a2cdfbc8e565c02ed769
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-10-06 19:33:47 +0300

    unhardcode!!! working!!! now can play charts

--- src/julibox.cr
+++ src/julibox.cr
@@ -1,11 +1,13 @@
 require "uri"
 require "json"
+require "uuid"
 require "http"
 require "http/server/handler"
 require "http/web_socket"
 
 class WSSender
-  @pc = 230
+  @pc = 1
+  @versions = {} of String => Int32
   
   def initialize(@ws : HTTP::WebSocket)
   end
@@ -18,14 +20,99 @@ class WSSender
     }.to_json)
     @pc += 1
   end
+  def send(opcode : String, key : String, val, version : Int32? = nil, from = 1)
+    version = version || @versions["key"]? || 0
+    send(opcode, {"key" => key, "val" => val, "version" => version, "from" => from})
+    @versions["key"] = version + 1
+  end
 end
 
 ws_handler = HTTP::WebSocketHandler.new do |ws, ctx|
+  q = ctx.request.query_params
+
+  role = q["role"]?
+  if role != "player"
+    raise "Role #{role} unsuppored"
+  end
+  player_name = q["name"]?.try &.upcase || "PLAYER"
+  user_id = q["user-id"]?.try { |s| UUID.new(s) } || UUID.random
+  format = q["format"]?
+  if format != "json"
+    raise "Format #{format} unsuppored"
+  end
+  
   d = WSSender.new ws
 
-  JSON.parse(File.read("src/nopus-opus_packets.json")).as_a.each do |obj|
-    d.send(obj["opcode"].as_s, obj["result"])
-  end
+  d.send("client/welcome", JSON.parse(
+    File.read("src/assets/lobby-sample.json")
+      # this isn't the worst way to do this! but it _is_ close
+      .sub("<PLAYER>", player_name)
+      # exclusively relevant to reconnections
+      .sub("<SECRET>", UUID.random)
+      # idk what this is based on
+      .sub("<DEVICEID>", "0366929ab8.8773612ac287d0d5de4a2a")
+  ))
+
+  chart = JSON.parse(File.read("src/assets/chart.json"))
+  # todo
+  selected_beatmap = chart["beatmaps"][0]
+
+  # todo: is sending both necessary?
+  d.send("object", "connectedPlayers", {"2" => {"avatar" => 0, "id" => 2, "inSong" => false, "name" => player_name}})
+  d.send("object", "connectedPlayers", {"2" => {"avatar" => 0, "beatmapSlug" => selected_beatmap["slug"], "id" => 2, "inSong" => false, "instrumentSlug" => chart["slug"], "name" => player_name}})
+
+  d.send("object", "beatmap:#{selected_beatmap["slug"]}", {
+    "config" => selected_beatmap,
+    "scaleKey" => chart["scaleKey"],
+    "scaleType" => chart["scaleType"],
+    "songName" => "Tutorial" # todo: how?
+  })
+  d.send("object", "instrument:#{chart["slug"]}", {
+    "config" => {
+      "capabilities" => ["Melodic", "Sustain"], # ???
+      "chains" => [
+        # todo: keysounds?
+      ] of Nil,
+      "slug" => chart["slug"]
+    },
+    "name" => "Tutorial Sampler" # ???
+  })
+  d.send("object", "guide", {
+    "guide" => chart["guide"]
+  })
+
+  d.send("object", "recording:2", {} of Nil => Nil)
+  d.send("object", "player:2", {
+    "guide" => {"$ref" => "guide"},
+    "kind" => "recording",
+    "recordingInfo" => {"$ref" => "recordingInfo"},
+    "responseEntity" => "recording:2"
+  })
+
+  d.send("object", "recordingInfo", {
+    "continuousCriteriaFactors" => [1,0.5,0.25,0],
+    "continuousPercentageCriteria" => [60,45,30],
+    "continuousSignalCriteria" => [0.03,0.08,0.12],
+    "countInIndex" => 1,
+    "discreteCriteria" => [80,120,160],
+    "duration" => chart["duration"],
+    "guideStartOffset" => chart["guideStartOffset"],
+    "inputWindow" => 160.0,
+    "maxDelay" => 15000,
+    "maxFlubs" => 100,
+    "minDisplayCombo" => 5,
+    "noteQualityScoreFactors" => [1,0.5,0.25,0],
+    "numCountInTaps" => 3,
+    "perfectModeIsOn" => false,
+    "position" => 0,
+    "preventFlubsIsOn" => false,
+    "recordingState" => "ResumeNeeded",
+    "scorePerHighestCombo" => 100.0,
+    "scorePoolPerDifficulty" => [100000,125000,150000,175000,200000],
+    "successPercentage" => 0.8,
+    "timeAfterCountInBeforeSong" => 4000.0,
+    "timeAfterCountInDuringSong" => 2500.0
+  })
 end
 
 class IndexHandler
@@ -70,7 +157,7 @@ class ProxyHandler
 
     if request.path.starts_with?("/api/")
       if request.path.starts_with?("/api/v2/rooms/")
-        json = JSON.parse(File.read("src/nopus-opus_game.json"))
+        json = JSON.parse(File.read("src/assets/game.json"))
       else
         json = JSON.parse(body)
         if !json["ok"].as_bool || !(json["body"]?.try &.as_h?)
@@ -120,6 +207,10 @@ class ProxyHandler
       context.response.headers.merge!(response.headers)
       # fuck cors
       context.response.headers["Access-Control-Allow-Origin"] = "*"
+      # don't pretend to send gzipped data
+      context.response.headers.delete("Content-Encoding")
+      # don't pretend to send chunked data
+      context.response.headers.delete("Transfer-Encoding")
 
       # intercept the body
       new_body = rewrite_body(req, response.body)
@@ -135,6 +226,8 @@ class ProxyHandler
         context.response.headers.merge!(response.headers)
         # fuck cors
         context.response.headers["Access-Control-Allow-Origin"] = "*"
+        # don't pretend to send chunked data
+        context.response.headers.delete("Transfer-Encoding")
 
         IO.copy(response.body_io, context.response)
 
