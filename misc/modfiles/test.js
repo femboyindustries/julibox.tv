9ac6b0d6bf6d57536ae60eec4d0c1c354e75340e
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-10-14 02:24:51 +0300

    not-dodoremi: completely redo eases

--- misc/modfiles/test.js
+++ misc/modfiles/test.js
@@ -1,14 +1,15 @@
 const m = $$m;
+const e = $$eases;
 
 m.set(0, 600, 'fov', 100, 'wave');
 
-//$$m.ease(0, 4, $$eases.easeOutCirc, 100, 'bumpy');
+//m.ease(0, 4, e.outCirc, 100, 'bumpy');
 
 let inv = false;
 for (let b = 0; b < 128; b += 2) {
   inv = !inv;
-  m.ease(b, 2, $$eases.easeOutExpo, inv ? 100 : 0, 'invert');
-  //m.add(b, 2, $$eases.easeOutCirc, 20, 'drunk');
+  m.ease(b, 2, e.outExpo, inv ? 100 : 0, 'invert');
+  //m.add(b, 2, e.outCirc, 20, 'drunk');
 }
 
-//m.ease(10, 20, $$eases.easeInOutQuad, -360, 'confusionoffsety', 360, 'rotationy')
+//m.ease(10, 20, e.inOutQuad, -360, 'confusionoffsety', 360, 'rotationy');
--- mods/not-dodoremi/src/ease.js
+++ mods/not-dodoremi/src/ease.js
@@ -1,135 +1,220 @@
-const $$_bounceOut = function (x) {
-	const n1 = 7.5625;
-	const d1 = 2.75;
+// js port of a lua port of an easing library:
+// https://github.com/XeroOl/notitg-mirin/blob/master/template/ease.lua
 
-	if (x < 1 / d1) {
-		return n1 * x * x;
-	} else if (x < 2 / d1) {
-		return n1 * (x -= 1.5 / d1) * x + 0.75;
-	} else if (x < 2.5 / d1) {
-		return n1 * (x -= 2.25 / d1) * x + 0.9375;
-	} else {
-		return n1 * (x -= 2.625 / d1) * x + 0.984375;
-	}
+let $$eases = {};
+
+if (true) {
+const e = $$eases;
+
+let sqrt = Math.sqrt;
+let sin = Math.sin;
+let asin = Math.asin;
+let cos = Math.cos;
+let pow = Math.pow;
+let exp = Math.exp;
+let pi = Math.PI;
+let abs = Math.abs;
+
+// Flip any easing function, making it go from 1 to 0
+e.flip = (fn) => (x) => 1 - fn(x);
+
+// Mix two easing functions together into a new ease
+// the new ease starts by acting like the first argument, and then ends like the second argument
+// Example: ease {0, 20, blendease(inQuad, outQuad), 100, 'modname'}
+e.blendease = (fn1, fn2) => (x) => {
+  let mixFactor = 3 * Math.pow(x, 2) - 2 * Math.pow(x, 3);
+  return (1 - mixFactor) * fn1(x) + mixFactor * fn2(x);
+}
+
+e.bounce = (t) => 4 * t * (1 - t)
+e.tri = (t) => 1 - abs(2 * t - 1);
+e.bell = (t) => inOutQuint(tri(t));
+e.pop = (t) => 3.5 * (1 - t) * (1 - t) * sqrt(t);
+e.tap = (t) => 3.5 * t * t * sqrt(1 - t);
+e.pulse = (t) => t < .5 ? e.tap(t * 2) : -e.pop(t * 2 - 1);
+
+e.spike = (t) => exp(-10 * abs(2 * t - 1));
+e.inverse = (t) => t * t * (1 - t) * (1 - t) / (0.5 - t);
+
+let popElasticInternal = (t, damp, count) =>
+	(1000 ^ -pow(t, damp) - 0.001) * sin(count * pi * t);
+
+let tapElasticInternal = (t, damp, count) =>
+	(1000 ^ -(pow(1 - t, damp)) - 0.001) * sin(count * pi * (1 - t));
+
+let pulseElasticInternal = (t, damp, count) =>
+	t < .5
+	  ? tapElasticInternal(t * 2, damp, count)
+	  : -popElasticInternal(t * 2 - 1, damp, count);
+
+e.popElastic = (x) => popElasticInternal(x, 1.4, 6);
+e.tapElastic = (x) => tapElasticInternal(x, 1.4, 6);
+e.pulseElastic = (x) => pulseElasticInternal(x, 1.4, 6);
+
+let impulseInternal = (t, damp) => {
+  t = pow(t, damp);
+  return t * (pow(1000, -t) - 0.001) * 18.6;
 };
 
-// https://github.com/ai/easings.net/blob/master/src/easings/easingsFunctions.ts#L33
-const $$eases = {
-	linear: (x) => x,
-	easeInQuad: function (x) {
-		return x * x;
-	},
-	easeOutQuad: function (x) {
-		return 1 - (1 - x) * (1 - x);
-	},
-	easeInOutQuad: function (x) {
-		return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
-	},
-	easeInCubic: function (x) {
-		return x * x * x;
-	},
-	easeOutCubic: function (x) {
-		return 1 - Math.pow(1 - x, 3);
-	},
-	easeInOutCubic: function (x) {
-		return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
-	},
-	easeInQuart: function (x) {
-		return x * x * x * x;
-	},
-	easeOutQuart: function (x) {
-		return 1 - Math.pow(1 - x, 4);
-	},
-	easeInOutQuart: function (x) {
-		return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
-	},
-	easeInQuint: function (x) {
-		return x * x * x * x * x;
-	},
-	easeOutQuint: function (x) {
-		return 1 - Math.pow(1 - x, 5);
-	},
-	easeInOutQuint: function (x) {
-		return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
-	},
-	easeInSine: function (x) {
-		return 1 - Math.cos((x * Math.PI) / 2);
-	},
-	easeOutSine: function (x) {
-		return Math.sin((x * Math.PI) / 2);
-	},
-	easeInOutSine: function (x) {
-		return -(Math.cos(Math.PI * x) - 1) / 2;
-	},
-	easeInExpo: function (x) {
-		return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
-	},
-	easeOutExpo: function (x) {
-		return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
-	},
-	easeInOutExpo: function (x) {
-		return x === 0
-			? 0
-			: x === 1
-			? 1
-			: x < 0.5
-			? Math.pow(2, 20 * x - 10) / 2
-			: (2 - Math.pow(2, -20 * x + 10)) / 2;
-	},
-	easeInCirc: function (x) {
-		return 1 - Math.sqrt(1 - Math.pow(x, 2));
-	},
-	easeOutCirc: function (x) {
-		return Math.sqrt(1 - Math.pow(x - 1, 2));
-	},
-	easeInOutCirc: function (x) {
-		return x < 0.5
-			? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
-			: (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
-	},
-	easeInBack: function (x) {
-		return c3 * x * x * x - c1 * x * x;
-	},
-	easeOutBack: function (x) {
-		return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
-	},
-	easeInOutBack: function (x) {
-		return x < 0.5
-			? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-			: (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
-	},
-	easeInElastic: function (x) {
-		return x === 0
-			? 0
-			: x === 1
-			? 1
-			: -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
-	},
-	easeOutElastic: function (x) {
-		return x === 0
-			? 0
-			: x === 1
-			? 1
-			: Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
-	},
-	easeInOutElastic: function (x) {
-		return x === 0
-			? 0
-			: x === 1
-			? 1
-			: x < 0.5
-			? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
-			: (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
-	},
-	easeInBounce: function (x) {
-		return 1 - $$_bounceOut(1 - x);
-	},
-	easeOutBounce: $$_bounceOut,
-	easeInOutBounce: function (x) {
-		return x < 0.5
-			? (1 - $$_bounceOut(1 - 2 * x)) / 2
-			: (1 + $$_bounceOut(2 * x - 1)) / 2;
-	},
+e.impulse = (x) => impulseInternal(x, 0.9);
 
-  instant: (_) => 1
+e.instant = () => 1;
+e.linear = (t) => t;
+e.inQuad = (t) => t * t;
+e.outQuad = (t) => -t * (t - 2);
+e.inOutQuad = (t) => {
+	t = t * 2;
+  return t < 1
+    ? 0.5 * pow(t, 2)
+    : 1 - 0.5 * pow(2 - t, 2);
 };
+e.outInQuad = (t) => {
+	t = t * 2;
+	return t < 1
+		? 0.5 - 0.5 * pow(1 - t, 2)
+	  : 0.5 + 0.5 * pow(t - 1, 2);
+};
+e.inCubic = (t) => t * t * t;
+e.outCubic = (t) => 1 - pow(1 - t, 3);
+e.inOutCubic = (t) => {
+	t = t * 2;
+	return t < 1
+		? 0.5 * pow(t, 3)
+	  : 1 - 0.5 * pow(2 - t, 3);
+};
+e.outInCubic = (t) => {
+	t = t * 2;
+	return t < 1
+		? 0.5 - 0.5 * pow(1 - t, 3)
+	  : 0.5 + 0.5 * pow(t - 1, 3);
+}
+e.inQuart = (t) => t * t * t * t;
+e.outQuart = (t) => 1 - pow(1 - t, 4);
+e.inOutQuart = (t) => {
+	t = t * 2;
+	return t < 1
+		? 0.5 * pow(t, 4)
+	  : 1 - 0.5 * pow(2 - t, 4);
+};
+e.outInQuart = (t) => {
+  t = t * 2;
+  return t < 1
+    ? 0.5 - 0.5 * pow(1 - t, 4)
+    : 0.5 + 0.5 * pow(t - 1, 4);
+};
+e.inQuint = (t) => pow(t, 5);
+e.outQuint = (t) => 1 - pow(1 - t, 5);
+e.inOutQuint = (t) => {
+  t = t * 2;
+  return t < 1
+    ? 0.5 * pow(t, 5)
+    : 1 - 0.5 * pow(2 - t, 5);
+};
+e.outInQuint = (t) => {
+  t = t * 2;
+  return t < 1
+    ? 0.5 - 0.5 * pow(1 - t, 5)
+    : 0.5 + 0.5 * pow(t - 1, 5);
+};
+e.inExpo = (t) => pow(1000, t - 1) - 0.001;
+e.outExpo = (t) => 1.001 - pow(1000, -t);
+e.inOutExpo = (t) => {
+  t = t * 2;
+  return t < 1
+    ? 0.5 * pow(1000, t - 1) - 0.0005
+    : 1.0005 - 0.5 * pow(1000, 1 - t);
+};
+e.outInExpo = (t) =>
+	t < 0.5
+		? outExpo(t * 2) * 0.5
+	  : inExpo(t * 2 - 1) * 0.5 + 0.5;
+
+e.inCirc = (t) => 1 - sqrt(1 - t * t);
+e.outCirc = (t) => sqrt(-t * t + 2 * t);
+e.inOutCirc = (t) => {
+	t = t * 2;
+	if (t < 1) {
+    return 0.5 - 0.5 * sqrt(1 - t * t)
+  } else {
+    t = t - 2
+    return 0.5 + 0.5 * sqrt(1 - t * t)
+  }
+};
+e.outInCirc = (t) =>
+	t < 0.5
+		? outCirc(t * 2) * 0.5
+	  : inCirc(t * 2 - 1) * 0.5 + 0.5;
+
+e.outBounce = (t) => {
+	if (t < 1 / 2.75)
+		return 7.5625 * t * t
+
+	if (t < 2 / 2.75) {
+		t = t - 1.5 / 2.75
+		return 7.5625 * t * t + 0.75
+  }
+
+	if (t < 2.5 / 2.75) {
+		t = t - 2.25 / 2.75
+		return 7.5625 * t * t + 0.9375
+  }
+
+	t = t - 2.625 / 2.75
+	return 7.5625 * t * t + 0.984375
+}
+
+e.inBounce = (t) => 1 - outBounce(1 - t);
+e.inOutBounce = (t) =>
+	t < 0.5
+		? inBounce(t * 2) * 0.5
+	  : outBounce(t * 2 - 1) * 0.5 + 0.5;
+e.outInBounce = (t) => 
+	t < 0.5
+		? outBounce(t * 2) * 0.5
+	  : inBounce(t * 2 - 1) * 0.5 + 0.5;
+
+e.inSine = (x) => 1 - cos(x * (pi * 0.5));
+e.outSine = (x) => sin(x * (pi * 0.5));
+e.inOutSine = (x) => 0.5 - 0.5 * cos(x * pi);
+e.outInSine = (t) =>
+	t < 0.5
+		? outSine(t * 2) * 0.5
+	  : inSine(t * 2 - 1) * 0.5 + 0.5;
+
+let outElasticInternal = (t, a, p) =>
+	a * pow(2, -10 * t) * sin((t - p / (2 * pi) * asin(1/a)) * 2 * pi / p) + 1;
+let inElasticInternal = (t, a, p) =>
+	1 - outElasticInternal(1 - t, a, p);
+let inOutElasticInternal = (t, a, p) =>
+	t < 0.5
+		? 0.5 * inElasticInternal(t * 2, a, p)
+		: 0.5 + 0.5 * outElasticInternal(t * 2 - 1, a, p);
+let outInElasticInternal = (t, a, p) =>
+	t < 0.5
+		? 0.5 * outElasticInternal(t * 2, a, p)
+		: 0.5 + 0.5 * inElasticInternal(t * 2 - 1, a, p);
+
+e.inElastic = (x) => inElasticInternal(x, 1, 0.3);
+e.outElastic = (x) => outElasticInternal(x, 1, 0.3);
+e.inOutElastic = (x) => inOutElasticInternal(x, 1, 0.3);
+e.outInElastic = (x) => outInElasticInternal(x, 1, 0.3);
+
+e.inBackInternal = (t, a) => t * t * (a * t + t - a);
+e.outBackInternal = (t, a) => {
+  t = t - 1;
+  return t * t * ((a + 1) * t + a) + 1;
+};
+e.inOutBackInternal = (t, a) =>
+	t < 0.5
+		? 0.5 * inBackInternal(t * 2, a)
+		: 0.5 + 0.5 * outBackInternal(t * 2 - 1, a);
+e.outInBackInternal = (t, a) =>
+	t < 0.5
+		? 0.5 * outBackInternal(t * 2, a)
+		: 0.5 + 0.5 * inBackInternal(t * 2 - 1, a);
+
+e.inBack = (x) => inBackInternal(x, 1.70158);
+e.outBack = (x) => outBackInternal(x, 1.70158);
+e.inOutBack = (x) => inOutBackInternal(x, 1.70158);
+e.outInBack = (x) => outInBackInternal(x, 1.70158);
+}
